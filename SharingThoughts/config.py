6b4a0897e46091a76d4f67ff1711b6d88f34a2f8
Sumendra2906 <55164121+Sumendra2906@users.noreply.github.com>
2022-04-08 20:42:14 +0530

    Update config.py

--- SharingThoughts/config.py
+++ SharingThoughts/config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config():
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = secrets.SECRET_KEY
     # database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = secrets.SQLALCHEMY_DATABASE_URI
     # for password reset
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = secrets.EMAIL_USER
+    MAIL_PASSWORD = secrets.EMAIL_PASS
